3b2485db46185d83c3a515a7f08d31b6995e15ac
kruffka <kruffko@gmail.com>
2022-03-01 01:37:23 +0700

    collors and makefile

--- complex/functions.h
+++ complex/functions.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define RED     "\033[1;31m"
+#define RESET   "\033[0m" // == in hex "\x1B[0m"
+#define BLACK   "\e[0;30m"
+#define WHITE   "\e[0;37m"
+
 typedef struct complex {
     int re;
     int im;
